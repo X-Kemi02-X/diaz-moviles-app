ac3f557394d946fb959151e6bc32e15f94062079
Kevin Diaz R <kevidiaz8@gmail.com>
2026-06-06 01:40:45 -0500

    fix: mostrar mensajes de error del servidor en registro

--- app/src/main/java/com/diazmoviles/app/data/repository/RegisterRepositoryImpl.kt
+++ app/src/main/java/com/diazmoviles/app/data/repository/RegisterRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.diazmoviles.app.data.repository
 import com.diazmoviles.app.data.remote.api.RegisterApi
 import com.diazmoviles.app.data.remote.api.RegisterRequest
 import com.diazmoviles.app.domain.repository.RegisterRepository
+import org.json.JSONObject
 import javax.inject.Inject
 
 class RegisterRepositoryImpl @Inject constructor(
@@ -18,8 +19,34 @@ class RegisterRepositoryImpl @Inject constructor(
             val response = registerApi.register(
                 RegisterRequest(username, password, email, nombre, apellido, cedula, telefono, direccion)
             )
-            if (!response.isSuccessful) {
-                throw Exception("Error al registrarse: ${response.code()}")
+            if (response.isSuccessful) {
+                Unit
+            } else {
+                val errorBody = response.errorBody()?.string()
+                val message = if (errorBody != null) {
+                    try {
+                        val json = JSONObject(errorBody)
+                        val keys = json.keys()
+                        val errors = mutableListOf<String>()
+                        while (keys.hasNext()) {
+                            val key = keys.next()
+                            val value = json.get(key)
+                            if (value is org.json.JSONArray) {
+                                for (i in 0 until value.length()) {
+                                    errors.add(value.getString(i))
+                                }
+                            } else {
+                                errors.add(value.toString())
+                            }
+                        }
+                        errors.joinToString("\n")
+                    } catch (_: Exception) {
+                        "Error al registrarse: ${response.code()}"
+                    }
+                } else {
+                    "Error al registrarse: ${response.code()}"
+                }
+                throw Exception(message)
             }
         }
     }
